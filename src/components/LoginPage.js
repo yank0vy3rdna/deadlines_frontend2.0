98b3aa647ff850bff7ef19af688c70d57c6dfff0
DeltaHeavyVIP <patutin.vovan@mail.ru>
2021-09-22 02:31:55 +0300

    V

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ import {
     MDBCardTitle,
     MDBCol,
     MDBInput,
-    MDBInputGroup,
+    MDBInputGroup, MDBInputGroupText,
     MDBRow
 } from "mdb-react-ui-kit";
 
@@ -16,10 +16,15 @@ function LoginPage() {
             <MDBCard center style={{maxWidth: '35rem'}}>
                 <MDBCardBody>
                     <MDBCardTitle>Login</MDBCardTitle>
+
                     <MDBCardText>
                         <MDBInputGroup className='mb-3'>
                             <MDBInput label='Email input' id='typeEmail' type='email'/>
+                        </MDBInputGroup>
+                        <MDBInputGroup className='mb-3'>
                             <MDBInput label='Password input' id='typePassword' type='password'/>
+                        </MDBInputGroup>
+                        <MDBInputGroup>
                             <MDBBtn>Login</MDBBtn>
                         </MDBInputGroup>
                     </MDBCardText>
