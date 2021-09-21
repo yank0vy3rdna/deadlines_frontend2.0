aa09243160e53e6e29689f5f0c7e5ff22f52e4f2
yank0vy3rdna <yankovyerdna@yandex.ru>
2021-09-22 01:54:08 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    MDBBtn, MDBCard, MDBCardBody, MDBCardText, MDBCardTitle,
+    MDBCol,
+    MDBCollapse,
+    MDBContainer, MDBDropdown, MDBDropdownItem, MDBDropdownLink, MDBDropdownMenu, MDBDropdownToggle,
+    MDBIcon,
+    MDBNavbar,
+    MDBNavbarBrand, MDBNavbarItem, MDBNavbarLink, MDBNavbarNav,
+    MDBNavbarToggler,
+    MDBRow
+} from "mdb-react-ui-kit";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showBasic, setShowBasic] = useState(false);
+
+    return (
+        <div>
+            {/*<MDBNavbar expand='lg' light bgColor='light'>*/}
+            {/*    <MDBContainer fluid>*/}
+            {/*        <MDBNavbarBrand href='#'>Deadlines</MDBNavbarBrand>*/}
+
+
+            {/*    </MDBContainer>*/}
+            {/*</MDBNavbar>*/}
+            <MDBContainer className="" style={{"height": "80vh"}}>
+                <MDBRow center style={{'height': '100%'}} className=''>
+                    <MDBCol center size='4' className='col-example '>
+                        <MDBCard center style={{maxWidth: '35rem'}}>
+                            <MDBCardBody>
+                                <MDBCardTitle>Deadlines</MDBCardTitle>
+                                <MDBCardText>
+                                    Deadlines - приложение для отслеживания дедлайнов студентами. Зарегистрируйся, найди
+                                    свою группу и получай оповещения о грядущих дедлайнах по email или telegram, или
+                                    создай собственную группу, пригласи одногруппников и помоги им отследить все
+                                    дедлайны
+
+
+                                </MDBCardText>
+                                <MDBBtn>Login</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </div>
+    );
 }
 
 export default App;
